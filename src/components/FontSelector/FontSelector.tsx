8dcefd70c10de5df2da3b4a01b30a7e6e2ca8757
seikou <seikou615@gmail.com>
2023-02-06 19:29:04 +0000

    fixed: font not changing globally once selected due to react not re-rendering

--- src/components/FontSelector/FontSelector.tsx
+++ src/components/FontSelector/FontSelector.tsx
@@ -1,38 +1,30 @@
-import {
-  Container,
-  DropDown,
-  Mono,
-  SansSerif,
-  Serif,
-  StyledButton,
-} from "./styles";
+import { Container, DropDown, Mono, SansSerif, Serif, StyledButton } from "./styles";
 import { EFonts, TFont } from "types";
 import React, { useEffect, useRef, useState } from "react";
 
-import { AnimatePresence } from "framer-motion";
 import { ArrowIcon } from "assets";
 import { ICommonProps } from "types";
-import { fade_in_out } from "global/animation";
 
 function FontSelector({ themeStyles, setThemeStyles }: ICommonProps) {
   const [currentFont, setCurrentFont] = useState<TFont>("Sans Serif");
   const [open, setOpen] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
+  
+  function changeFont(font: TFont) {
+    const newThemeStyles = {...themeStyles};
 
-
-  useEffect(() => {
-
-    const newThemeStyles = themeStyles;
-
-    newThemeStyles.dark.font = EFonts[currentFont];
-    newThemeStyles.light.font = EFonts[currentFont];
+    newThemeStyles.dark.font = EFonts[font];
+    newThemeStyles.light.font = EFonts[font];
 
 
     setThemeStyles(newThemeStyles);
-    setOpen(false);
+    setOpen(false)
+  }
 
+  useEffect(() => {
+    changeFont(currentFont);
   }, [currentFont]);
-
+  
 
   function handleClickOutside(event: any) {
     if (modalRef.current && !modalRef.current.contains(event.target)) {
@@ -49,23 +41,19 @@ function FontSelector({ themeStyles, setThemeStyles }: ICommonProps) {
 
   return (
     <Container ref={modalRef}>
-      <StyledButton className={open ? "open" : ''} onClick={() => setOpen((e) => !e)}>
+      <StyledButton onClick={() => setOpen(e => !e)}>
         {currentFont}
 
         <ArrowIcon />
       </StyledButton>
 
-      <AnimatePresence>
-        {open && (
-          <DropDown variants={fade_in_out} initial="hidden" animate="visible" exit="exit" >
-            <SansSerif onClick={() => setCurrentFont("Sans Serif")}>
-              Sans Serif
-            </SansSerif>
-            <Serif onClick={() => setCurrentFont("Serif")}>Serif</Serif>
-            <Mono onClick={() => setCurrentFont("Mono")}>Mono</Mono>
-          </DropDown>
-        )}
-      </AnimatePresence>
+      {open && (
+        <DropDown>
+          <SansSerif onClick={() => setCurrentFont("Sans Serif")}>Sans Serif</SansSerif>
+          <Serif onClick={() => setCurrentFont("Serif")}>Serif</Serif>
+          <Mono onClick={() => setCurrentFont("Mono")}>Mono</Mono>
+        </DropDown>
+      )}
     </Container>
   );
 }
